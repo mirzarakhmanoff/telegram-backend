3e7c1bd22746c5b387fabc751b3e101c0bdf8c66
Ulugbek <mirzarakhmanoff@gmail.com>
2024-12-18 13:52:25 +0500

    add some updates

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
+const errorMiddleware = require("./middlewares/error.middleware");
 
 const app = express();
 
@@ -9,7 +11,17 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", require("./routes/index"));
+app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 6000;
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const bootstrap = async () => {
+  try {
+    const PORT = process.env.PORT || 6000;
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("MongoDB connected"));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+bootstrap();
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,7 +1,21 @@
+const BaseError = require("../errors/base.error");
+const userModel = require("../models/user.model");
+
 class AuthController {
   async login(req, res, next) {
-    const { email } = req.body;
-    res.json({ email });
+    try {
+      const { email } = req.body;
+      const existUser = userModel.findOne({ email });
+      if (existUser) {
+        throw BaseError.BadRequest("User already exists", [
+          { email: "User already exists" },
+        ]);
+      }
+      const createUser = await userModel.create({ email });
+      res.status(201).json(createUser);
+    } catch (error) {
+      next(error);
+    }
   }
   async verify(req, res, next) {
     const { email, otp } = req.body;
